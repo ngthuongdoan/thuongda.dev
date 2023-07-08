fecbb6297ceb90de65983d6e879b808ecc10d695
Eddie <thuong.doanngoc@hellohealthgroup.com>
2023-07-08 18:55:58 +0700

    fix: adapt to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/css/prism.css'
+import 'katex/dist/katex.css'
+// import '@fontsource/inter/variable-full.css'
+import '../css/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
+
+import siteMetadata from '@/data/siteMetadata'
+import Analytics from 'components/analytics'
+import LayoutWrapper from 'components/LayoutWrapper'
+import { ClientReload } from 'components/ClientReload'
+import { AppProps } from 'next/app'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isSocket = process.env.SOCKET
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      {/* {isDevelopment && isSocket && <ClientReload />} */}
+      <Analytics />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
+  )
 }
-
-export default MyApp
